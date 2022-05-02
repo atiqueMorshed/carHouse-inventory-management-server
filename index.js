66b7a7c7a7226d86f0105f6e22e0ec4f8a655aed
Atique Morshed <iamsamix17@gmail.com>
2022-05-03 04:23:11 +0600

    Adds car to inventory collections. Verifies JWT and compares with current user before writing to DB

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const uri = `mongodb+srv://warehouse-management-admin:${[
 app.use(express.json());
 app.use(cors());
 
+const validateJWT = (req, res, next) => {
+  const token = req?.headers?.authorization.split(' ')[1];
+
+  if (!token) return res.status(401).send({ message: 'Unauthorized Access' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).send({ message: 'Forbidden Access!' });
+    if (decoded) {
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
 // Mongo API
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -25,15 +39,66 @@ const run = async () => {
   try {
     await client.connect();
     const inventoryCollection = client.db('carHouse').collection('inventory');
+    const sliderCollection = client.db('carHouse').collection('slider');
+
     console.log('DB CONNECTED.');
 
+    // Generates JWT after successful login
     app.post('/api/login', (req, res) => {
-      const { email } = req.body;
-      const accessToken = jwt.sign({ email }, process.env.JWT_SECRET, {
+      const { email, uid } = req.body;
+      const accessToken = jwt.sign({ email, uid }, process.env.JWT_SECRET, {
         expiresIn: '1d',
       });
 
-      res.send({ accessToken });
+      return res.status(201).send({ accessToken });
+    });
+
+    // Adds new car
+    app.post('/api/addCar', validateJWT, async (req, res) => {
+      const { carData } = req?.body;
+      const decodedUid = req?.decoded?.uid;
+
+      if (!decodedUid || decodedUid !== carData?.supplier?.uid) {
+        return res.status(403).send({ message: 'Forbidden Access.' });
+      }
+
+      if (
+        !carData?.carName ||
+        !carData?.price ||
+        !carData?.quantity ||
+        !carData?.imageURL ||
+        !carData?.description ||
+        !carData?.supplier ||
+        !carData?.supplier?.name ||
+        !carData?.supplier?.uid
+      ) {
+        return res.status(400).send({ message: 'Insufficient form data.' });
+      }
+
+      let result;
+      try {
+        result = await inventoryCollection.insertOne(carData);
+      } catch (error) {
+        return res.status(400).send({ message: 'Insertion failed.' });
+      }
+
+      // car was successfully added to the inventory and now, we will add it to slider if isSlider was ticked from addCars form
+      if (result?.acknowledged && result?.insertedId && carData?.isSlider) {
+        const carSliderInfo = {
+          carName: carData.carName,
+          supplier: carData.supplier,
+          imageURL: carData.imageURL,
+          carId: result.insertedId,
+        };
+        try {
+          const sliderResult = await sliderCollection.insertOne(carSliderInfo);
+          return res.status(201).send(sliderResult);
+        } catch (error) {
+          res.status(400).send({ message: 'Insertion failed.' });
+        }
+      } else {
+        return res.status(201).send({ ...result, sliderInsertionFailed: true });
+      }
     });
   } finally {
   }
